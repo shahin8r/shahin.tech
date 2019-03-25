c9cd7577156a49db73b382bd2519727018075323
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2019-03-25 11:25:08 +0100

    added built-with page component

---
+++ src/pages/built-with.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../layout'
+
+export default () => (
+  <Layout siteTitle="Made with and for <strong>open-source</strong>.">
+      <h2>Want juicy tech details?</h2>
+
+      <p>This website is built with GatsbyJS. A wonderful new framework for React. You might have wondered how this site is so blazing fast? Well, thank Gatsby for that.</p>
+  </Layout>
+)
