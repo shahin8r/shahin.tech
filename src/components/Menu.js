7febcb21379cdad09236d70b41008ee6319f3c56
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2019-03-25 11:15:53 +0100

    added menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Styled from 'styled-components'
+import { Link } from 'gatsby'
+
+import Colors from '../colors'
+
+const Menu = Styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  a {
+    text-decoration: none;
+
+    &.active li {
+      color: ${Colors.text};
+    }
+  }
+`
+
+const MenuItem = Styled.li`
+  line-height: 150%;
+  color: ${Colors.menu};
+  font-size: 2.5rem;
+  font-weight: bold;
+
+  &:hover {
+    color: ${Colors.text}
+  }
+`
+
+const partiallyActive = () => ({ isPartiallyCurrent }) => ({
+  className: isPartiallyCurrent ? ` active` : ``
+})
+
+export default () => (
+  <Menu>
+    <Link activeClassName="active" to="/"><MenuItem>About</MenuItem></Link>
+    <Link activeClassName="active" to="/blog" getProps={partiallyActive('active')}><MenuItem>Blog</MenuItem></Link>
+    <Link activeClassName="active" to="/work"><MenuItem>Work</MenuItem></Link>
+    <Link activeClassName="active" to="/social"><MenuItem>Social</MenuItem></Link>
+  </Menu>
+)
